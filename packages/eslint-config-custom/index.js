80b69aaa85e854f604d019ae87714feb6e6559f8
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2023-02-13 20:44:25 +0100

    init of api
    config files for prettier and eslint updated as well

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,1 +1,13 @@
-module.exports = {};
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'turbo', 'prettier'],
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2020
+	},
+	rules: {
+		'@typescript-eslint/no-explicit-any': 'error',
+		'@typescript-eslint/no-unused-vars': 'error'
+	},
+	ignorePatterns: ['node_modules', 'dist']
+};
