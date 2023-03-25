5078e8ca5f8a2519de30dbdf7f3b03f7be7aa5da
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2023-03-25 13:43:36 +0100

    update scheduled db update function

--- apps/api/src/providers/schedule/schedule.provider.ts
+++ apps/api/src/providers/schedule/schedule.provider.ts
@@ -13,47 +13,61 @@ export class ScheduleProvider {
 	private readonly logger = new Logger(ScheduleProvider.name);
 
 	async updateDatabase() {
-		// TODO: If vastrafikId already exists, update instead of create. Try bulkInsert instead of create. Clean up function.
+		// TODO: If vastrafikId already exists, update instead of create. If id in database but not in response, delete it from db. Try bulkInsert instead of create. Clean up function.
+		/*
+		1. Get all stop areas
+		2. For each stop area, create station (if not exists)
+		3. For each stop area, create geometry (if not exists)
+		4. For each stop area, get all stop points
+		5. For each stop point, create position (if not exists)
+		6. For each stop point, create geometry (if not exists)
+		7. If any ids in database but not in response, delete them from db
+		*/
 		this.logger.log('Scheduled update of database started');
 
 		try {
 			const stopAreas = await this.vasttrafikProvider.getStopAreas();
 
-			for (const stopArea of stopAreas) {
-				const stationGeometry: GeometryCreationAttributes = {
-					longitude: stopArea.geometry.eastingCoordinate,
-					latitude: stopArea.geometry.northingCoordinate
-				};
+			const stationVasttrafikIds = [];
+			const positionVasttrafikIds = [];
 
-				const station: StationCreationAttributes = {
+			for (const stopArea of stopAreas) {
+				stationVasttrafikIds.push(stopArea.gid);
+
+				const station = await this.databaseProvider.upsertStation({
 					vasttrafikId: stopArea.gid,
 					name: stopArea.name,
 					shortName: stopArea.shortName,
-					abbreviation: stopArea.abbreviation,
-					geometryId: (await this.databaseProvider.createGeometry(stationGeometry)).id
-				};
+					abbreviation: stopArea.abbreviation
+				});
 
-				const createdStation = await this.databaseProvider.createStation(station);
+				await this.databaseProvider.upsertGeometry({
+					longitude: stopArea.geometry.eastingCoordinate,
+					latitude: stopArea.geometry.northingCoordinate,
+					stationId: station.id
+				});
 
 				for (const stopPoint of stopArea.stopPoints) {
-					const positionGeometry: GeometryCreationAttributes = {
-						longitude: stopPoint.geometry.eastingCoordinate,
-						latitude: stopPoint.geometry.northingCoordinate
-					};
+					positionVasttrafikIds.push(stopPoint.gid);
 
-					const position: PositionCreationAttributes = {
+					const position = await this.databaseProvider.upsertPosition({
 						vasttrafikId: stopPoint.gid,
 						name: stopPoint.name,
 						shortName: stopPoint.shortName,
 						abbreviation: stopPoint.abbreviation,
 						designation: stopPoint.designation,
-						geometryId: (await this.databaseProvider.createGeometry(positionGeometry)).id,
-						stationId: createdStation.id
-					};
+						stationId: station.id
+					});
 
-					await this.databaseProvider.createPosition(position);
+					await this.databaseProvider.upsertGeometry({
+						longitude: stopPoint.geometry.eastingCoordinate,
+						latitude: stopPoint.geometry.northingCoordinate,
+						positionId: position.id
+					});
 				}
 			}
+
+			// TODO: Delete stations and positions that are not in the response from Västtrafik
 		} catch (error) {
 			this.logger.error('Scheduled database update failed', error);
 		} finally {
