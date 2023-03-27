052b7d96d5cee8399c5b3ed69c72ead28e1c3646
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2023-03-27 19:42:04 +0200

    create theme provider
    Will be used for managing different themes for the client, e.g. light and dark mode

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -2,12 +2,15 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { useTheme } from './providers/theme.provider';
+import { Theme } from './types/theme.type';
 
 function App() {
 	const [count, setCount] = useState(0);
+	const [theme, setTheme] = useTheme();
 
 	return (
-		<div className="App">
+		<div className="App bg-white dark:bg-black">
 			<div>
 				<a href="https://vitejs.dev" target="_blank">
 					<img src={viteLogo} className="logo" alt="Vite logo" />
@@ -15,6 +18,14 @@ function App() {
 				<a href="https://reactjs.org" target="_blank">
 					<img src={reactLogo} className="logo react" alt="React logo" />
 				</a>
+				<button
+					className="bg-blue-500 dark:bg-red-500 text-white font-bold py-2 px-4 rounded"
+					onClick={() => {
+						setTheme(theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT);
+					}}
+				>
+					Toggle theme
+				</button>
 			</div>
 			<h1>Vite + React</h1>
 			<div className="card">
---
+++ apps/client/src/providers/theme.provider.tsx
@@ -0,0 +1,46 @@
+// External dependencies
+import { createContext, useContext, useState, useEffect } from 'react';
+
+// Internal dependencies
+import { Theme } from '$src/types/theme.type';
+
+const ThemeContext = createContext({} as [Theme | undefined, React.Dispatch<React.SetStateAction<Theme | undefined>>]);
+
+export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+	const [theme, setTheme] = useState<Theme | undefined>(undefined);
+
+	// Initial theme load from local storage
+	useEffect(() => {
+		const localTheme = localStorage.getItem('theme');
+
+		// If the stored theme is valid, set it as the current theme
+		if (Object.values(Theme).includes(localTheme as Theme)) {
+			setTheme(localTheme as Theme);
+		} else {
+			// Otherwise, set the theme to light
+			setTheme(Theme.LIGHT);
+		}
+	}, []);
+
+	// Update local storage and the body class when theme changes
+	useEffect(() => {
+		if (!theme) return;
+
+		localStorage.setItem('theme', theme);
+
+		document.body.classList.remove(Theme.LIGHT, Theme.DARK);
+		document.body.classList.add(theme);
+	}, [theme]);
+
+	return <ThemeContext.Provider value={[theme, setTheme]}>{children}</ThemeContext.Provider>;
+};
+
+export const useTheme = () => {
+	const context = useContext(ThemeContext);
+
+	if (!context) {
+		throw new Error('useTheme must be used within a ThemeProvider');
+	}
+
+	return context;
+};
