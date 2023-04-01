2da38f8079017b8820fb580f98cb8dfe43f68d2b
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2023-04-01 17:38:43 +0200

    Update Layout.tsx

--- apps/client/src/components/Layout.tsx
+++ apps/client/src/components/Layout.tsx
@@ -1,20 +1,13 @@
-import clsx from 'clsx';
-
-const styles = {
-	size: 'w-screen h-screen p-2 overflow-hidden',
-	grid: 'grid grid-cols-1 sm:grid-cols-[50%_50%] md:grid-cols-[20rem_auto_20rem] lg:grid-cols-[25rem_auto_25rem]'
-};
-
 interface LayoutProps extends React.HTMLAttributes<HTMLDivElement> {
 	children: React.ReactNode;
 }
 
 function Layout({ children, ...props }: LayoutProps) {
-	const classes = clsx(styles.size, styles.grid);
-
 	return (
-		<div className={classes} {...props}>
-			{children}
+		<div className="p-2 w-screen h-screen">
+			<div className="relative w-full h-full" {...props}>
+				{children}
+			</div>
 		</div>
 	);
 }
