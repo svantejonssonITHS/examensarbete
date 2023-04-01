ae3efe17a3ccbd6520cb110764303b53ba96c4f5
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2023-04-01 18:13:49 +0200

    Update Layout.tsx

--- apps/client/src/components/Layout.tsx
+++ apps/client/src/components/Layout.tsx
@@ -4,7 +4,7 @@ interface LayoutProps extends React.HTMLAttributes<HTMLDivElement> {
 
 function Layout({ children, ...props }: LayoutProps) {
 	return (
-		<div className="p-2 w-screen h-screen">
+		<div className="p-2 w-screen h-screen overflow-hidden">
 			<div className="relative w-full h-full" {...props}>
 				{children}
 			</div>
