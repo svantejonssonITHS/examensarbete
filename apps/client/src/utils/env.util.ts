13cdb5b0d8f555700adc400e8f0d279338df3f2c
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2023-04-16 11:11:02 +0200

    update env config

---
+++ apps/client/src/utils/env.util.ts
@@ -0,0 +1,5 @@
+export default {
+	AUTH0_DOMAIN: import.meta.env.VITE_AUTH0_DOMAIN,
+	AUTH0_CLIENT_ID: import.meta.env.VITE_AUTH0_CLIENT_ID,
+	AUTH0_REDIRECT_URI: import.meta.env.VITE_AUTH0_REDIRECT_URI
+};
